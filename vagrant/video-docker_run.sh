35dd9868beb524d729ab0c82f333b62025d67651
Luigi Dorigatti <54908603+doricats@users.noreply.github.com>
2021-07-19 14:58:49 +0200

    Update video-docker_run.sh

--- vagrant/video-docker_run.sh
+++ vagrant/video-docker_run.sh
@@ -61,11 +61,11 @@ echo "|                                                    STARTING VIDEO STREAM
 echo "+----------------------------------------------------------------------------------------------------------------------------------+"
 
 echo "[TCP]: Video streaming"
-docker exec -d tcp-video ffmpeg -re -stream_loop -1 -i /root/big_buck_bunny_720p_10mb.mp4 -vcodec libx264 -vprofile baseline -g 30 -acodec aac -strict -2 -loop -10 -f flv rtmp://localhost/show/stream
+docker exec -d tcp-video ffmpeg -re -stream_loop -1 -i /root/videotest.mp4 -vcodec libx264 -vprofile baseline -g 30 -acodec aac -strict -2 -loop -10 -f flv rtmp://localhost/show/stream
 
 echo "[HTTP/2]: Video streaming"
-docker exec -d http2-video ffmpeg -re -stream_loop -1 -i /root/big_buck_bunny_720p_10mb.mp4 -vcodec libx264 -vprofile baseline -g 30 -acodec aac -strict -2 -loop -10 -f flv rtmp://localhost/show/stream
+docker exec -d http2-video ffmpeg -re -stream_loop -1 -i /root/videotest.mp4 -vcodec libx264 -vprofile baseline -g 30 -acodec aac -strict -2 -loop -10 -f flv rtmp://localhost/show/stream
 
 echo "[HTTP/3]: Video streaming"
-docker exec -d http3-video ffmpeg -re -stream_loop -1 -i /root/big_buck_bunny_720p_10mb.mp4 -vcodec libx264 -vprofile baseline -g 30 -acodec aac -strict -2 -loop -10 -f flv rtmp://localhost/show/stream
+docker exec -d http3-video ffmpeg -re -stream_loop -1 -i /root/videotest.mp4 -vcodec libx264 -vprofile baseline -g 30 -acodec aac -strict -2 -loop -10 -f flv rtmp://localhost/show/stream
                                                                                                                      
